04a989f4b7c373e03e721695c0f4820c4d777765
Brice Maron <bmaron@synthesio.com>
2017-12-08 21:33:04 +0100

    refactor(clock): move from class to data-attr

--- js/main.js
+++ js/main.js
@@ -1,25 +1,17 @@
 (function() {
   "use strict";
 
-  function removeClass() {
-    $(".number").removeClass(function(index, className) {
-      return (className.match(/\bnumber-\S+/g) || []).join(" ");
-    });
-  }
-
   function addTime(elementClass, digitsType) {
     // Convert to string to know str length
     digitsType = digitsType.toString();
     // add zero padding if < 0
     digitsType = ("00" + digitsType).substring(digitsType.length);
 
-    $(elementClass + "0").addClass("number-" + digitsType[0]);
-    $(elementClass + "1").addClass("number-" + digitsType[1]);
+    $(elementClass + "0").attr("data-number", digitsType[0]);
+    $(elementClass + "1").attr("data-number", digitsType[1]);
   }
 
   function pulse() {
-    removeClass();
-
     var d = new Date();
     addTime(".second-", d.getSeconds());
     addTime(".minute-", d.getMinutes());
